a2bab74b4c756ab9a02e72a6246bf45e6f8b854b
wasif zaman <wasif.zaman1@students.mq.edu.au>
2023-04-15 21:28:36 +1000

    created options

---
+++ gift/data.js
@@ -0,0 +1,28 @@
+help = ["No ayuda, adiós?", "adiós", "github", "map"];
+
+github = [
+  "cd",
+  "git clone",
+  "ls",
+  "git status",
+  "git add",
+  "git commit",
+  "git push",
+  "git pull",
+  "git branch",
+  "git merge",,
+  "git log",
+  "git clone",
+  "git init",
+  "git config",
+  "git add .",
+  
+];
+
+fail = [
+  "¿Qué?",
+  "maybe choose option DUMBASS",
+  "god your are hopeless choose an option",
+];
+
+// Path: gift\index.js
