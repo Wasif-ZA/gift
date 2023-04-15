5143259a1d6462a1bb000129316d69df475edfea
wasif zaman <wasif.zaman1@students.mq.edu.au>
2023-04-15 22:03:34 +1000

    not even working chatgpt base

--- gift/main.js
+++ gift/main.js
@@ -1,24 +1,70 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './data.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.addEventListener("keydown", (event) => {
 
-setupCounter(document.querySelector('#counter'))
+  if (event === 'enter') {
+    
+  }
+
+})
+import { help, github, fail } from "./data.js";
+
+// Define the command prompt and terminal area
+const prompt = document.querySelector(".prompt");
+const terminal = document.querySelector(".terminal");
+
+// Define the cards area
+const cards = document.querySelector("#cards");
+
+// Add an event listener for key presses
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    // Get the command entered by the user
+    const command = prompt.textContent.trim();
+
+    // Create a new line in the terminal with the command
+    const newLine = document.createElement("div");
+    newLine.textContent = `$ ${command}`;
+    terminal.appendChild(newLine);
+
+    // Handle different commands
+    switch (command) {
+      case "help":
+        // Create a new card for the help data
+        help.forEach((item) => {
+          const card = createCard(item);
+          cards.appendChild(card);
+        });
+        break;
+      case "github":
+        // Create a new card for the github data
+        github.forEach((item) => {
+          const card = createCard(item);
+          cards.appendChild(card);
+        });
+        break;
+      default:
+        // Create a new card for the fail data
+        fail.forEach((item) => {
+          const card = createCard(item);
+          cards.appendChild(card);
+        });
+        break;
+    }
+
+    // Clear the command prompt
+    prompt.textContent = "";
+  }
+});
+
+// Function to create a new card
+function createCard(text) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const cardText = document.createElement("p");
+  cardText.textContent = text;
+  card.appendChild(cardText);
+
+  return card;
+}
